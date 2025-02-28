e582cd1591c3d82170b114a05188d6348063efbc
exuteboll <exuteboll4@gmail.com>
2025-02-28 12:23:08 +0200

    Add files via upload

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+         string brand;
+         int years;
+        public static int kolvo = 0;
+
+        public string Brand { get { return brand; } set { brand = value; } }
+        public int Years { get { return years; } set { years = value; } }
+
+        public Car() { }
+        public Car(string brand, int years)
+        {
+            this.Brand = brand;
+            this.Years = years;
+            
+            kolvo++;
+        }
+
+        //public void ShowInfo()
+        //{
+        //    Console.WriteLine($"Бренд: {brand}\n Год: {years}\n Количество машин: {kolvo}");
+        //}
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car car = new Car();
+            //car.Brand = "Дигули";
+            //car.Years = 2005;
+
+            //car.ShowInfo();
+            //Console.WriteLine("Введите марку бренд");
+            //string brand1 = Console.ReadLine();
+            //int years1 = int.Parse(Console.ReadLine());
+
+            //Car car1 = new Car(brand1, years1);
+            //car1.ShowInfo();
+            //Car car2 = new Car("БМВ", 2000);
+            //car2.ShowInfo();
+            Car car12 = new Car("Жигули",25000);
+            Car car13 = new Car("влопа", 214);
+            Car car14 = new Car("asd", 124);
+            Console.WriteLine($"{car12.Brand}{car12.Years},{Car.kolvo}");
+            Console.ReadKey();
+        }
+    }
+}
